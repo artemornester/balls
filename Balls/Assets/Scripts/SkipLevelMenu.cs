3c40a34d62b00035e2067407b7f9a7ed5a6e58a9
Nester <artemornester@gmail.com>
2022-08-01 20:38:55 +0300

    Add skip menu

---
+++ Balls/Assets/Scripts/SkipLevelMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SkipLevelMenu : MonoBehaviour
+{
+    public PlayerState playerState;
+    public GameConfig gameConfig;
+    public Text levelText;
+
+    int level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localPosition = Vector3.zero;
+        level = playerState.level;
+        int visibleLevel = level + 1;
+        levelText.text = visibleLevel.ToString();
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenMenu()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void lowerLevel()
+    {
+        level--;
+        int visibleLevel = level + 1;
+        levelText.text = visibleLevel.ToString();
+    }
+
+    public void highterLevel()
+    {
+        level++;
+        int visibleLevel = level + 1;
+        levelText.text = visibleLevel.ToString();
+    }
+
+    public void Go()
+    {
+        playerState.level = level;
+        playerState.Save();
+        SceneManager.LoadScene(1);
+    }
+}
